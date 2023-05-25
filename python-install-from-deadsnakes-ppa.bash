f9f1ea62d660f76545cce357b61277137e99db0e
sammtcbn <sammtcbn@gmail.com>
2023-05-25 17:09:49 +0800

    add script to install newer python version for old Ubuntu version

---
+++ python-install-from-deadsnakes-ppa.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script is useful for updating python version on Ubuntu 18.04 .
+# Because my default python version is v3.6.9 on my Ubuntu 18.04
+# refer to https://tecadmin.net/how-to-install-python-3-9-on-ubuntu-18-04/
+# refer to https://www.itsupportwale.com/blog/how-to-upgrade-to-python-3-9-0-on-ubuntu-18-04-lts/
+sudo apt -y update
+sudo apt-get -y install software-properties-common
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt -y update
+sudo apt-get -y install python3.9
+
+sudo update-alternatives --list python3
+
+sudo update-alternatives --remove-all python3
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 0
+sudo update-alternatives --config python3
+
+sudo update-alternatives --list python3
