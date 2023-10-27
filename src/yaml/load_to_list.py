f3a995bf41d7342e243440dfaf5d1e0c7e997ac3
sammtcbn <sammtcbn@gmail.com>
2023-10-27 21:57:09 +0800

    sample for loading yaml to list

---
+++ src/yaml/load_to_list.py
@@ -0,0 +1,21 @@
+import yaml
+
+def main():
+    with open("example.yaml", "r") as f:
+        try:
+            content = yaml.safe_load(f)
+            keylist = []
+            valuelist = []
+            for k in content['fruits'].keys():
+                keylist.append(k)
+            for v in content['fruits'].values():
+                valuelist.append(v)
+            print(keylist)
+            # ['apple', 'pear', 'banana']
+            print(valuelist)
+            # ['red', 'green', 'yellow']
+        except yaml.YAMLError as exc:
+            print(exc)
+
+if __name__ == '__main__':
+    main()
