e9c4c3751c9cfca8121bd771f8a6d1af56e52996
sammtcbn <sammtcbn@gmail.com>
2023-11-07 16:50:07 +0800

    ssh execute command example

---
+++ src/ssh/ssh_execute_cmd.py
@@ -0,0 +1,29 @@
+# pip install paramiko
+import paramiko
+
+def ssh_run_cmd(ip, user, pwd, cmdstr):
+    ret = False
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        print("Connecting to {}".format(ip))
+        ssh.connect(ip, username=user, password=pwd)
+        channel = ssh.invoke_shell()
+        if channel:
+            stdin, stdout, stderr = ssh.exec_command(cmdstr)
+            ret = True
+    except Exception as e:
+        print(e)
+
+    return ret
+
+def main():
+    serverip = "192.168.0.4"
+    userid = "sam"
+    userpwd = "1234"
+    ssh_run_cmd(serverip, userid, userpwd, "rm /tmp/report.txt")
+
+if __name__ == '__main__':
+    main()
