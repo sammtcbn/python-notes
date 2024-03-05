2f6d9d1c856a0c5526b903b16bec7fb8e54e6f5a
sammtcbn <sammtcbn@gmail.com>
2024-03-05 13:47:07 +0800

    argc example

--- src/sys_argc_argv.py
+++ src/sys_argc_argv.py
@@ -5,7 +5,9 @@ def main():
 	firstname = sys.argv[1]
 	lastname = sys.argv[2]
 	age = sys.argv[3]
+	argc = len(sys.argv)
 
+	print("argc " + str(argc))
 	print("process name " + procname)
 	print("first name " + firstname)
 	print("last name " + lastname)
@@ -14,7 +16,8 @@ def main():
 main()
 
 # Result:
-# $python sys_argv.py sam lin 10
+# $python sys_argc_argv.py sam lin 10
+# argc 4
 # process name sys_argv.py
 # first name sam
 # last name lin
