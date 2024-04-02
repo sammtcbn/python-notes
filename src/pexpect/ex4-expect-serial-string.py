2a428825daecb11da8431b44f6e1d32292040e96
sammtcbn <sammtcbn@gmail.com>
2024-04-02 18:02:05 +0800

    error handle for opening serial port

--- src/pexpect/ex4-expect-serial-string.py
+++ src/pexpect/ex4-expect-serial-string.py
@@ -5,18 +5,27 @@ import pexpect.popen_spawn
 
 
 def main():
-    with serial.Serial('/dev/ttyUSB0', 115200, timeout=0) as ser:
-        session = pexpect.fdpexpect.fdspawn(ser, timeout=10)
+    try:
+        print("Open serial port ...")
+        ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=0)
+    except:
+        sys.exit ("Error opening port")
 
-        try:
-            session.expect('Power On')
-            print("got Power On")
-            session.expect('System is ready')
-            print("got System is ready")
-        except Exception as e:
-            print(f"An exception occurred: {e}")
-            #print(e)
+    print("Serial port opened")
 
+    session = pexpect.fdpexpect.fdspawn(ser, timeout=10)
+
+    try:
+        session.expect('Power On')
+        print("got Power On")
+        session.expect('System is ready')
+        print("got System is ready")
+    except Exception as e:
+        print(f"An exception occurred: {e}")
+        #print(e)
+
+    ser.close()
+    print("port is closed")
 
 if __name__ == '__main__':
     main()
