f434877d422a2950732a2ff5260d371b17a7fa12
sammtcbn <sammtcbn@gmail.com>
2024-04-03 17:20:50 +0800

    strip method example

--- src/loop_ex1.py
+++ src/loop_ex1.py
@@ -2,4 +2,23 @@ m=int(input("enter a number:"))
 sum=0
 for i in range (1, m+1):
     sum=sum+i
+    print("i=",i,"sum=",sum)
 print("sum=",sum)
+
+"""
+Result:
+
+$ python loop_ex1.py
+enter a number:10
+i= 1 sum= 1
+i= 2 sum= 3
+i= 3 sum= 6
+i= 4 sum= 10
+i= 5 sum= 15
+i= 6 sum= 21
+i= 7 sum= 28
+i= 8 sum= 36
+i= 9 sum= 45
+i= 10 sum= 55
+sum= 55
+"""
---
+++ src/string/strip_ex.py
@@ -0,0 +1,16 @@
+# strip() method - Remove the leading and trailing characters
+
+str1 = "000abcd0sam0001230000"
+print(str1.strip('0'))
+
+str2 = "   Sam  Lin       "
+print(str2.strip())
+
+"""
+Result:
+
+$ python strip_ex.py
+abcd0sam000123
+Sam  Lin
+
+"""
