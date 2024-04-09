c51b0071d8df2c2b716c1bddc19874771b8a08ea
sammtcbn <sammtcbn@gmail.com>
2024-04-09 15:19:04 +0800

    format function example

---
+++ src/built-in_functions/format_ex.py
@@ -0,0 +1,9 @@
+str = "Hello, {}. Good morning. Your id is {}"
+
+username = "Sam"
+userid = 89
+
+str2 = str.format(username, 89)
+
+print(str)  # Hello, {}. Good morning. Your id is {}
+print(str2) # Hello, Sam. Good morning. Your id is 89
--- src/os/base-name.py
+++ src/os/base-name.py
@@ -1,14 +1,18 @@
 import os
 
 def main():
-	base_name = os.path.basename(__file__)
-	print ('base name is ' + base_name)
+    base_name = os.path.basename(__file__)
+    print ('base name is ' + base_name)
 
-	log_name = os.path.basename(__file__).split('.')[0] + '.log'
-	print ('log name is ' + log_name)
+    log_name = os.path.basename(__file__).split('.')[0] + '.log'
+    print ('log filename is ' + log_name)
 
-	log_full_path = os.path.join(os.getcwd(), log_name)
-	print ('log full path is ' + log_full_path)
+    log_full_path = os.path.join(os.getcwd(), log_name)
+    print ('log full path is ' + log_full_path)
+
+    # in one line
+    filename = os.path.join(os.getcwd(), os.path.basename(__file__).split('.')[0]+'.log')
+    print ('log full path is ' + filename)
 
 if __name__ == '__main__':
 	main()
