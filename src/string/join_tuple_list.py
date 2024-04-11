e74f69bb046cea737e01716bd69719a86e49b9ff
sammtcbn <sammtcbn@gmail.com>
2024-04-11 15:32:45 +0800

    join ex

---
+++ src/string/join_tuple_list.py
@@ -0,0 +1,14 @@
+myTuple = ("Sam", "Terry", "Alvin", "Carol")
+print(myTuple)              #('Sam', 'Terry', 'Alvin', 'Carol')
+print(type(myTuple))        #<class 'tuple'> 
+t = "#".join(myTuple)
+print(t)                    #Sam#Terry#Alvin#Carol
+print(type(t))              #<class 'str'>
+
+myList = ["Sam", "Terry", "Alvin", "Carol"]
+print(myList)               #('Sam', 'Terry', 'Alvin', 'Carol')
+print(type(myList))         #<class 'list'>
+l = ".".join(myList)
+print(l)                    #Sam.Terry.Alvin.Carol
+print(type(l))              #<class 'str'>  
+
