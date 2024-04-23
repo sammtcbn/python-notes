32c992a49c717ee9aff4ae82278ffeda06e79b94
sammtcbn <sammtcbn@gmail.com>
2024-04-23 14:05:16 +0800

    log example update

--- src/logging/log_ex3.py
+++ src/logging/log_ex3.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import time
 
 def log_init():
     logfilename = os.path.join(os.getcwd(), os.path.basename(__file__).split('.')[0]+'.log')
@@ -13,11 +14,26 @@ def log_init():
         ]
     )
 
+def log_level_set(level):
+    # Get the root logger
+    root_logger = logging.getLogger()
+    root_logger.setLevel(level)
+
 def main():
     log_init()
 
     logging.debug('This is a debug message')
     logging.info('This is an info message')
+    logging.warning('This is a warning message')
+    logging.error('This is an error message')
+    logging.critical('This is a critical error message')
+
+    time.sleep(3)
+
+    log_level_set(logging.WARNING)
+
+    logging.debug('This is a debug message')      # This won't be displayed
+    logging.info('This is an info message')       # This won't be displayed
     logging.warning('This is a warning message')
     logging.error('This is an error message')
     logging.critical('This is a critical error message')
